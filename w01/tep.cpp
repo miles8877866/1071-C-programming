549ca7aa5521b95f528fabb9b20e757d6a3aa19f
miles8877866 <miles8877866@gmail.com>
2018-09-17 22:46:28 +0800

    Adding w01 files

---
+++ w01/tep.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+
+
+int main(){
+	float C,F;
+	printf("Enter temperature in C:");
+	scanf("%f",&C);
+	F=C*9/5+32;
+	printf("%.1f F=%.1f C",F,C);
+	
+	
+} 
---
+++ w01/welcome.cpp
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main()
+{printf("welcome to promming\n");
+printf("My name:³¯¬fèû\n");
+return 0;
+}
