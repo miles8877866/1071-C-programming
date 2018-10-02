e7c150a2d20d75bc420e042cc98f74a1ef6bff71
miles8877866 <43271915+miles8877866@users.noreply.github.com>
2018-10-02 17:47:24 +0800

    Create menu2.cpp

---
+++ w03/menu2.cpp
@@ -0,0 +1,86 @@
+#include<stdio.h>
+
+void drowrect(int height, int width, int filled)
+{
+    if(filled)
+    {
+        for(int i=1; i<=height; i++)
+        {
+            for(int j=1; j<=width; j++)
+
+            {
+                printf("*",i,j);
+            }
+            printf("\n");
+
+        }
+    }
+
+    else
+    {
+        for(int i=1; i<=height; i++)
+        {
+            for(int j=1; j<=width; j++)
+            {
+                if(i==1 || i==height || j==1 || j==width)
+                    printf("*");
+                else
+                    printf(" ");
+            }
+            printf("\n");
+        }
+    }
+
+}
+
+
+
+int main()
+{
+    int choice;
+    int height;
+    int width;
+    int type;
+    int filled;
+
+    while(1)
+    {
+        printf("Main menu\n");
+        printf("1. Rectangle\n");
+        printf("2. Vertical triangle\n");
+        printf("3. Exits\n");
+        printf("=> ");
+        scanf("%d", &choice);
+        if(choice==3)
+            break;
+        switch(choice)
+        {
+        case 1:
+            printf("Enter height,width,filled: ");
+            scanf("%d %d %d", &height, &width, &filled);
+            drowrect(height, width, filled);
+            break;
+        case 2:
+            switch(type)
+            {
+            case 1:
+                printf("Enter height and type: ");
+                scanf("%d %d", &height, &type);
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 4:
+
+                break;
+            }
+
+        }
+    }
+
+
+    return 0;
+}
